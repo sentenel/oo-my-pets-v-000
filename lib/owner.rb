d41674fa64bbce84652b36e66991786d1ce55509
Seth Wax <sethlwax@gmail.com>
2018-04-07 21:02:14 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,33 +26,37 @@ class Owner
   end
 
   def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
+    pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
+    pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    @pets[:dogs].each {|dog| dog.mood = "happy"}
+    pets[:dogs].each {|dog| dog.mood = "happy"}
   end
 
   def play_with_cats
-    @pets[:cats].each {|cat| cat.mood = "happy"}
+    pets[:cats].each {|cat| cat.mood = "happy"}
   end
 
   def feed_fish
-    @pets[:fishes].each {|fish| fish.mood = "happy"}
+    pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
   def sell_pets
-    @pets.each do |species, pets|
-      pets.each {|pet| pet.mood = "nervous"}
-      pets.clear
+    pets.each do |species, pets_arr|
+      pets_arr.each {|pet| pet.mood = "nervous"}
+      pets_arr.clear
     end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
 end
